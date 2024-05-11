27228613651d7eea3c1e0fab7dcf531d1eb82d42
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-05-11 13:20:58 -0300

    Initial commit

---
+++ apps/docs/app/getting-started/from-example/page.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/ui"
+
+export default function Page(): JSX.Element {
+	return (
+		<main className={'grid grid-cols-1 gap-4 max-w-screen-sm mx-auto place-content-center min-h-screen p-8'}>
+			<Card>
+				<CardHeader>
+					<CardTitle>
+						Getting started
+					</CardTitle>
+
+					<CardDescription>
+						Templates
+					</CardDescription>
+				</CardHeader>
+
+				<CardContent>
+					<p>Choose from template</p>
+				</CardContent>
+			</Card>
+		</main>
+	)
+}
