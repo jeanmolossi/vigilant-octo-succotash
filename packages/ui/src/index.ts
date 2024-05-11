d7471484992101ac6955467fdb900fbb672646ad
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-05-11 14:57:42 -0300

    feat: added some components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,19 @@
-import './styles.css';
+import './styles.css'
 
 // components
-export * from './button';
-export * from './card';
+export * from './avatar'
+export * from './button'
+export * from './card'
+export * from './input'
+export * from './label'
+export * from './form'
+export * from './navigation-menu'
+export * from './command'
+export * from './dialog'
+export * from './popover'
+export * from './toggle-group'
+export * from './toggle'
+export * from './select'
 
 // providers
-export * from './theme-provider';
+export * from './theme-provider'
